72cb30518a7afb28f2205a4bb0d4ad2d8c80010c
Spror <wiktorspringer27@gmail.com>
2023-01-03 20:31:27 +0100

    adding basic main and opticalFlow declaration

--- main.cpp
+++ main.cpp
@@ -1,61 +1,70 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <stdlib.h>
 #include <chrono>
 #include <functional>
+#include <iostream>
 #include <numeric>
+#include <stdlib.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/video.hpp>
 #include <opencv2/cudaarithm.hpp>
 #include <opencv2/cudaimgproc.hpp>
-#include <opencv2/cudawarping.hpp>
 #include <opencv2/cudaoptflow.hpp>
+#include <opencv2/cudawarping.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/videoio.hpp>
 
+void opticalFlow(cv::VideoCapture  cap, std::string device);
 
-using namespace cv;
-int main( int argc, const char** argv )
-{
-    Mat frame;
-    //--- INITIALIZE VIDEOCAPTURE
-    VideoCapture cap;
-    // open the default camera using default API
-    // cap.open(0);
-    // OR advance usage: select any API backend
-    int deviceID = 0;             // 0 = open default camera
-    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-    // open selected camera using selected API
-    cap.open(deviceID, apiID);
-    // check if we succeeded
-    if (!cap.isOpened()) {
-        std::cerr << "ERROR! Unable to open camera\n";
-        return -1;
+int main(int argc, const char **argv) {
+
+  std::string device = "cpu";
+
+  if (argc == 2) {
+
+    if (argv[1] == "cpu" || argv[1] == "gpu") {
+      device = argv[1];
+    } else {
+      std::cerr << "ERROR! Wrong option during called" << std::endl;
+      return (-1);
     }
-    //--- GRAB AND WRITE LOOP
-    std::cout << "Start grabbing" << std::endl
-        << "Press any key to terminate" << std::endl;
-    for (;;)
-    {
-        // wait for a new frame from camera and store it into 'frame'
-        cap.read(frame);
-        // check if we succeeded
-        if (frame.empty()) {
-            std::cerr << "ERROR! blank frame grabbed\n";
-            break;
-        }
-        // show live and wait for a key with timeout long enough to show images
-        imshow("Live", frame);
-        if (waitKey(5) >= 0)
-            break;
-    }
-    // the camera will be deinitialized automatically in VideoCapture destructor
-    return 0;
+  }
 
+  std::cout << "Configuration:" << std::endl;
+  std::cout << "device: " << device << std::endl;
 
+  cv::Mat frame;
+  cv::VideoCapture cap;
+
+  int deviceID = 0; // 0 = Deafult camera
+  int apiID = 0;    // 0 = autodetect defaul API
+
+  cap.open(deviceID, apiID);
+
+  if (!cap.isOpened()) {
+    std::cerr << "ERROR! Unable to open camera" << std::endl;
+    exit(-1);
+  }
+
+  std::cout << "Start grabbing" << std::endl
+            << "Press any key to terminate" << std::endl;
+
+//   for (;;) {
+//     // wait for a new frame from camera and store it into 'frame'
+//     cap.read(frame);
+//     // check if we succeeded
+//     if (frame.empty()) {
+//       std::cerr << "ERROR! blank frame grabbed\n";
+//       break;
+//     }
+//     // show live and wait for a key with timeout long enough to show images
+//     imshow("Live", frame);
+//     if (cv::waitKey(5) >= 0)
+//       break;
+//   }
+
+  return 0;
 }
-
